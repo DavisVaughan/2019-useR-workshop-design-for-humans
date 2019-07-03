e9bac078d61751b3eb8a38a366aaac5597fe08b8
DavisVaughan <davis@rstudio.com>
2019-07-03 12:15:05 -0400

    Update fit interface example

--- examples/02-1-fit-interface.R
+++ examples/02-1-fit-interface.R
@@ -4,6 +4,8 @@ library(recipes)
 # formula method
 logistic_regression(mpg ~ ., mtcars)
 
+logistic_regression(mpg ~ ., mtcars, intercept = FALSE)
+
 # robustness on `y`
 logistic_regression(mtcars, "x")
 
@@ -13,3 +15,9 @@ rec <- recipe(Sepal.Length ~ Species + Sepal.Width, iris) %>%
   step_log(Sepal.Width)
 
 logistic_regression(rec, iris)
+
+# recipes
+rec_intercept <- recipe(mpg ~ ., mtcars) %>%
+  step_intercept()
+
+logistic_regression(rec_intercept, mtcars)
