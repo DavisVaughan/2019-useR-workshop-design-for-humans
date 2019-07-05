d69be8f8406e196c0d5edb6c1ecd3b4c7cf65ab9
DavisVaughan <davis@rstudio.com>
2019-07-05 17:17:59 -0400

    Add sens/spec yardstick examples

--- examples/07-predict-bridge.R
+++ examples/07-predict-bridge.R
@@ -44,3 +44,30 @@ test_with_hard_predictions
 
 test_with_hard_predictions %>%
   accuracy(admit, .pred_class)
+
+# ------------------------------------------------------------------------------
+# Sensitivity / Specificity
+
+# out of all of the true rejections, how many did i get right?
+test_with_hard_predictions %>%
+  sens(admit, .pred_class)
+
+# confirm
+test_with_hard_predictions %>% 
+  summarise(
+    n_true_rejects = sum(admit == 0),
+    n_correctly_pred_reject = sum(admit == 0 & .pred_class == 0),
+    n_correctly_pred_reject / n_true_rejects
+  )
+
+# out of all of the true admittances, how many did i get right?
+test_with_hard_predictions %>%
+  spec(admit, .pred_class)
+
+# confirm
+test_with_hard_predictions %>% 
+  summarise(
+    n_true_admit = sum(admit == 1),
+    n_correctly_pred_admit = sum(admit == 1 & .pred_class == 1),
+    n_correctly_pred_admit / n_true_admit
+  )
